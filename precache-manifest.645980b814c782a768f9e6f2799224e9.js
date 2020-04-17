48090cebfd81c37ff9877e3be0c97b0acbe16470
Duong Dat <tao@cu8-3.local>
2020-04-17 14:18:57 +0700

    ok

---
+++ precache-manifest.645980b814c782a768f9e6f2799224e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab3c3bb3e2c85ae1501d8f0938f8d2d4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3b8e3640e89791ba12a8",
+    "url": "./static/css/main.9f0fedff.chunk.css"
+  },
+  {
+    "revision": "1ccb42dbb88dedf7d445",
+    "url": "./static/js/2.be8b24bf.chunk.js"
+  },
+  {
+    "revision": "d9b445fd12d8bcaca03d304ff543ec4e",
+    "url": "./static/js/2.be8b24bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b8e3640e89791ba12a8",
+    "url": "./static/js/main.80fd997e.chunk.js"
+  },
+  {
+    "revision": "35c27fd6939ca3911ae5",
+    "url": "./static/js/runtime-main.58a15421.js"
+  }
+]);
